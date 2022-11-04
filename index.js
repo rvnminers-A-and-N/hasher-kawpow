c52a90955d3e71d853d6f13dfa1be1e1c0cb028e
rvnminers-A-and-N <86896075+rvnminers-A-and-N@users.noreply.github.com>
2022-11-04 17:46:36 -0400

    Evrprogpow

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const kawpow = require('bindings')('hasherkawpow.node');
+const evrprogpow = require('bindings')('hasherevrprogpow.node');
 
 module.exports = {
 
@@ -45,7 +45,7 @@ function hashOne(headerHashBuf, nonceBuf, blockHeight, mixOutBuf, hashOutBuf) {
     _expectBuffer(mixOutBuf, 'mixOutBuf', 32);
     _expectBuffer(hashOutBuf, 'hashOutBuf', 32);
 
-    kawpow.hash_one(headerHashBuf, nonceBuf, blockHeight, mixOutBuf, hashOutBuf);
+    evrprogpow.hash_one(headerHashBuf, nonceBuf, blockHeight, mixOutBuf, hashOutBuf);
 }
 
 
@@ -57,7 +57,7 @@ function verify(headerHashBuf, nonceBuf, blockHeight, mixHashBuf, hashOutBuf) {
     _expectBuffer(mixHashBuf, 'mixOutBuf', 32);
     _expectBuffer(hashOutBuf, 'hashOutBuf', 32);
 
-    return kawpow.verify(headerHashBuf, nonceBuf, blockHeight, mixHashBuf, hashOutBuf);
+    return evrprogpow.verify(headerHashBuf, nonceBuf, blockHeight, mixHashBuf, hashOutBuf);
 }
 
 
